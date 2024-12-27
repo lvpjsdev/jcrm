23381f4c2aee1562ac217c80daa2fc725516d901
Leonid Petrov <lp.msarchitect@gmail.com>
2024-12-27 22:15:25 +0400

    feat(back): add express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('BOOm!');
+import express from 'express';
+
+const expressApp = express();
+
+expressApp.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+expressApp.listen(3000, () => {
+  console.log('Server started on port 3000');
+});

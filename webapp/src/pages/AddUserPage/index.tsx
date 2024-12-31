d18a3e9c872fff49b414484a2dfa528016e26474
Leonid Petrov <lp.msarchitect@gmail.com>
2024-12-31 15:32:49 +0400

    feat(webapp): add formik

--- webapp/src/pages/AddUserPage/index.tsx
+++ webapp/src/pages/AddUserPage/index.tsx
@@ -1,10 +1,32 @@
-export const AddUserPage = () => (
-  <section>
-    <h1>Add user</h1>
+import { useFormik } from 'formik';
+
+export const AddUserPage = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      telegram: '',
+      email: '',
+      startDate: '',
+      period: 0,
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  return (
     <section>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis dicta voluptatibus
-      obcaecati totam laudantium possimus provident tempore fugit dolor. Sequi molestiae ipsa
-      doloribus et dolorem autem aperiam distinctio ullam voluptates?
+      <h1>Add user</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <button type="submit">Add</button>
+      </form>
     </section>
-  </section>
-);
+  );
+};

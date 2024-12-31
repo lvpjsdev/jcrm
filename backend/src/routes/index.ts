1e9cf80a6388670349ab3eb81c7017813bc161df
Leonid Petrov <lp.msarchitect@gmail.com>
2024-12-31 18:37:48 +0400

    feat(ext): add generate index extension

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,14 @@
 import { trpc } from '../lib/trpc';
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TRPCRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
 import { getUserTRPCRoute } from './getUser';
-import { getUsersListTRPCRoute } from './getUserList';
+import { getUsersListTRPCRoute } from './getUsersList';
+// @endindex
 
 export const trpcRouter = trpc.router({
-  getUsersList: getUsersListTRPCRoute,
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TRPCRoute,`)
   getUser: getUserTRPCRoute,
+  getUsersList: getUsersListTRPCRoute,
+  // @endindex
 });
 
 export type TRPCRouter = typeof trpcRouter;

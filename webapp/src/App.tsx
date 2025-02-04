ae625c64b0ad0302783cf0b84d88aabf7ee64c0c
Leonid Petrov <lvpjsdev@gmail.com>
2025-02-04 19:14:23 +0300

    feat(webapp): add mantine ui kit

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -5,18 +5,30 @@ import { User } from './entities/User';
 import { Layout } from './layouts/Layout/Layout';
 import './app/styles/global.scss';
 import { AddUserPage } from './pages/AddUserPage';
-import { UsersPage } from './pages/users';
+import { UsersPage } from './pages/UsersList';
+import { createTheme, MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export const App = () => (
-    <TRPCProvider>
+  <TRPCProvider>
+    <MantineProvider theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
             <Route path={routes.getViewAllUsersRoute()} element={<UsersPage />} />
             <Route path={routes.getViewAddUserRoute()} element={<AddUserPage />} />
-            <Route path={routes.getViewUserRoute(routes.viewUsersRouteParams)} element={<User />} />
+            <Route
+              path={routes.getViewUserRoute(routes.viewUsersRouteParams)}
+              element={<User />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
-    </TRPCProvider>
-  );
+    </MantineProvider>
+  </TRPCProvider>
+);

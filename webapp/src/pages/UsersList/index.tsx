6db26e4aa64e99e66025ef0a47021018f6ccc2c3
Leonid Petrov <lvpjsdev@gmail.com>
2025-02-20 21:33:07 +0300

    feat(webapp): add infinity loading by button click

--- webapp/src/pages/UsersList/index.tsx
+++ webapp/src/pages/UsersList/index.tsx
@@ -1,33 +1,61 @@
+/* eslint-disable complexity */
+import { Alert } from '@mantine/core';
 import { Link } from 'react-router';
 import { getViewUserRoute } from '../../app/routes';
 import { trpc } from '../../app/trpc';
 
 export const UsersPage = () => {
-  const { isLoading, isError, isFetching, data, error } = trpc.getUsersList.useQuery();
-
-  if (isLoading || isFetching) {
-    return <span>Loading...</span>;
-  }
-
-  if (isError) {
-    return (
-      <>
-        <span>{`Something goes wrong :( ...`}</span>
-        <pre>{JSON.stringify(error, null, 2)}</pre>
-      </>
-    );
-  }
+  const {
+    isLoading,
+    isError,
+    data,
+    error,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+    isRefetching,
+  } = trpc.getUsersList.useInfiniteQuery(
+    {
+      limit: 2,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
 
   return (
     <>
       <h1>Users</h1>
-      <ul>
-        {data?.map((user) => (
-          <li key={user.id}>
-            <Link to={getViewUserRoute({ userId: user.id })}>{user.telegram}</Link>
-          </li>
-        ))}
-      </ul>
+      {isLoading || isRefetching ? (
+        <div>Loading...</div>
+      ) : isError ? (
+        <Alert color="red" title="Error">
+          {JSON.stringify(error, null, 2)}
+        </Alert>
+      ) : (
+        <>
+          <ul>
+            {data?.pages
+              .flatMap((page) => page.users)
+              .map((user) => (
+                <li key={user.id}>
+                  <Link to={getViewUserRoute({ userId: user.id })}>{user.telegram}</Link>
+                </li>
+              ))}
+          </ul>
+          {hasNextPage && !isFetchingNextPage && (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                void fetchNextPage();
+              }}
+            >
+              Loading more
+            </button>
+          )}
+          {isFetchingNextPage && <span>Loading...</span>}
+        </>
+      )}
     </>
   );
 };
